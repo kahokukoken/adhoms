15406897bc86b8285e36279276b92e5333c5ae1e
kahokukoken <kahokukoken@gmail.com>
2026-09-17 07:56:33 +0900

    test: accept concrete municipal job titles

--- tests/playtest.spec.js
+++ tests/playtest.spec.js
@@ -45,7 +45,7 @@ test.describe('ADHOMS TGS Playtest', () => {
         const text = await cards.nth(i).innerText();
         expect(text).toMatch(/\d{2}歳|年齢不詳|組織アカウント|SYSTEM/);
         if (!/組織アカウント|SYSTEM/.test(text)) {
-          expect(text).toMatch(/主婦|会社員|店主|職員|農家|高校生|VTuber|動画配信者|医師|教員|記者|自治会|運転手|介護|研究者/);
+          expect(text).toMatch(/主婦|会社員|店主|職員|係長|主査|農家|高校生|VTuber|動画配信者|医師|教員|記者|自治会|運転手|介護|研究者/);
         }
       }
       if (m < 11) {
